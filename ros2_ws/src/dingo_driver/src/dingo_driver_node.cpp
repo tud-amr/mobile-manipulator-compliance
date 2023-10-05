af6b220faaf0ec726d769835d009a3c6b5685c4d
Jelmer de Wolde <jsdewolde@gmail.com>
2023-10-05 13:22:27 +0000

    Introduce service.

--- ros2_ws/src/dingo_driver/src/dingo_driver_node.cpp
+++ ros2_ws/src/dingo_driver/src/dingo_driver_node.cpp
@@ -5,6 +5,7 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "dingo_driver_msg/msg/feedback.hpp"
+#include "dingo_driver_msg/srv/set_gain.hpp"
 #include "dingo_driver.h"
 
 class DingoDriverNode : public rclcpp::Node
@@ -21,6 +22,13 @@ public:
         driver_manager_.connect_gateway();
         driver_manager_.add_actuator(5, "rear_right_wheel");
         driver_manager_.initialize_encoders();
+    }
+
+    void set_gain(const std::shared_ptr<dingo_driver_msg::srv::SetGain::Request> request,
+                  std::shared_ptr<dingo_driver_msg::srv::SetGain::Response> response)
+    {
+        response->set__success(true);
+        RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Service called.");
     }
 
     void canread_loop()
@@ -50,6 +58,7 @@ public:
 private:
     rclcpp::Publisher<dingo_driver_msg::msg::Feedback>::SharedPtr publisher_;
     dingo_driver::DriverManager driver_manager_;
+    rclcpp::Service<dingo_driver_msg::srv::SetGain>::SharedPtr service_ = this->create_service<dingo_driver_msg::srv::SetGain>("set_gain", std::bind(&DingoDriverNode::set_gain, this, std::placeholders::_1, std::placeholders::_2));
 };
 
 int main(int argc, char *argv[])
