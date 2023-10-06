361d3647eb2acbfaabe3b7d9d17ac772d9acf858
Jelmer de Wolde <jsdewolde@gmail.com>
2023-10-06 11:18:16 +0000

    Change command rate.

--- ros2_ws/src/dingo_driver/src/dingo_driver_node.cpp
+++ ros2_ws/src/dingo_driver/src/dingo_driver_node.cpp
@@ -59,7 +59,7 @@ public:
                 continue;
             driver_manager_.command("rear_right_wheel", "Cur", command_);
             command_ = 0;
-            std::this_thread::sleep_for(std::chrono::milliseconds(1));
+            std::this_thread::sleep_for(std::chrono::milliseconds(10));
         }
     }
     void feedback_loop()
