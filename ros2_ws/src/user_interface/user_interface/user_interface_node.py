33d134bd9230f59dcb675840e666d6a58a3ebfe5
Jelmer de Wolde <jsdewolde@gmail.com>
2023-10-18 13:05:54 +0000

    Add combined user interface for arm and base.

---
+++ ros2_ws/src/user_interface/user_interface/user_interface_node.py
@@ -0,0 +1,95 @@
+import os
+import rclpy
+from rclpy.node import Node
+from kinova_driver_msg.msg import JointFeedback, KinovaFeedback, JointState, KinovaState
+from dingo_driver_msg.msg import WheelFeedback, DingoFeedback
+from kinova_driver_msg.srv import Service
+from compliant_control.user_interface import UserInterface
+from threading import Thread
+import time
+
+
+class UserInterfaceNode(Node):
+    """A node that starts the user interface."""
+
+    def __init__(self) -> None:
+        super().__init__("user_interface_node")
+        self.create_subscription(
+            KinovaFeedback, "/kinova/feedback", self.kinova_feedback, 10
+        )
+        self.create_subscription(
+            DingoFeedback, "/dingo/feedback", self.dingo_feedback, 10
+        )
+        self.create_subscription(KinovaState, "/kinova/state", self.kinova_state, 10)
+        self.client = self.create_client(Service, "/kinova/service")
+
+        self.start_position = None
+        self.interface = UserInterface(self.callback)
+        spin_thread = Thread(target=self.start_spin_loop)
+        spin_thread.start()
+        initialize_thread = Thread(target=self.callback, args=["Initialize"])
+        initialize_thread.start()
+        self.interface.start_render_loop()
+
+    def callback(self, name: str) -> None:
+        """Connect the interface with service calls."""
+        self.interface.mode = "waiting"
+        self.interface.update_control()
+        request = Service.Request()
+        request.name = name
+        future = self.client.call_async(request)
+        while not future.done():
+            time.sleep(0.1)
+        self.interface.mode = future.result().mode
+        self.interface.update_control()
+
+    def kinova_feedback(self, msg: KinovaFeedback) -> None:
+        """Update the Kinova feedback."""
+        for joint in self.interface.joints:
+            joint_feedback: JointFeedback = getattr(msg, joint.name)
+            joint.position = joint_feedback.position
+            joint.speed = joint_feedback.speed
+            joint.current = joint_feedback.current
+        self.interface.update_rate = msg.update_rate
+        self.interface.update_kinova_plots()
+
+    def dingo_feedback(self, msg: DingoFeedback) -> None:
+        """Update the Dingo feedback."""
+        for wheel in self.interface.wheels:
+            wheel_feedback: WheelFeedback = getattr(msg, wheel.name)
+            wheel.position = wheel_feedback.position + 1.0
+            wheel.speed = wheel_feedback.speed
+            wheel.power = wheel_feedback.current * wheel_feedback.voltage
+        self.interface.update_dingo_plots()
+
+    def kinova_state(self, msg: KinovaState) -> None:
+        """Update the Kinova state."""
+        for joint in self.interface.joints:
+            joint_state: JointState = getattr(msg, joint.name)
+            joint.active = joint_state.active
+            joint.mode = joint_state.mode[:3]
+            joint.ratio = joint_state.ratio
+            joint.fric_d = joint_state.fric_d
+            joint.fric_s = joint_state.fric_s
+        self.interface.mode = msg.mode
+        self.interface.servoing = msg.servoing
+        self.interface.compensate_friction = msg.compensate_friction
+        self.interface.automove_target = msg.automove_target
+        self.interface.update_control()
+        self.interface.update_state()
+
+    def start_spin_loop(self) -> None:
+        """Start the ros spin loop."""
+        rclpy.spin(self)
+
+
+def main(args: any = None) -> None:
+    """Main."""
+    rclpy.init(args=args)
+    UserInterfaceNode()
+    rclpy.shutdown()
+    os._exit(0)
+
+
+if __name__ == "__main__":
+    main()
