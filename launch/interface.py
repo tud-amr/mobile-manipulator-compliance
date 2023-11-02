a2d7719c2adc0b404891347eece53a27a7cbb71a
Jelmer de Wolde <jsdewolde@gmail.com>
2023-11-02 15:31:38 +0000

    Create launcher for interface only.

---
+++ launch/interface.py
@@ -0,0 +1,18 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description() -> None:
+    """Generate the launch description."""
+    return LaunchDescription(
+        [
+            Node(
+                package="visualization",
+                executable="visualization_node",
+            ),
+            Node(
+                package="user_interface",
+                executable="user_interface_node",
+            ),
+        ]
+    )
