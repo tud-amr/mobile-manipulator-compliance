e07f6254a5c841b6ebde24d5c08051944037c842
Jelmer de Wolde <jsdewolde@gmail.com>
2023-12-14 09:59:38 +0000

    Remove Z from target mover.

--- python/compliant_control/mujoco/target_mover.py
+++ python/compliant_control/mujoco/target_mover.py
@@ -4,10 +4,7 @@ from threading import Thread
 import time
 
 STEP_ANGLE = np.deg2rad(0.2)
-STEP_Z = 0.0005
-ANGLE_MAX = np.deg2rad(130)
-Z0 = 0.4
-Z_MAX = 0.1
+ANGLE_MAX = np.deg2rad(90)
 
 
 class TargetMover:
@@ -26,18 +23,15 @@ class TargetMover:
 
     def move_target_loop(self) -> None:
         """A loop that moves the target."""
+        x, y, z = self.get_target()
         while self.active:
-            x, y, z = self.get_target()
             r = np.linalg.norm(np.sqrt(x**2 + y**2))
             angle = np.arctan2(y, x)
             if abs(angle) > ANGLE_MAX and self.rot_direction == np.sign(angle):
                 self.rot_direction *= -1
-            if abs(z - Z0) > Z_MAX and self.z_direction == np.sign(z - Z0):
-                self.z_direction *= -1
             new_angle = angle + self.rot_direction * STEP_ANGLE
             x = np.cos(new_angle) * r
             y = np.sin(new_angle) * r
-            z += self.z_direction * STEP_Z
             self.update_target([x, y, z])
             time.sleep(0.01)
 
