1ee3342de4716c92939f2d2c10b0063b76109b50
Jelmer de Wolde <jsdewolde@gmail.com>
2024-01-09 15:49:00 +0000

    Create ros packages in ros1.

---
+++ ros/noetic/src/control_interface/control_interface/control_interface_node.py
@@ -0,0 +1,239 @@
+from __future__ import annotations
+import rospy
+import os
+import time
+import signal
+import sys
+
+from threading import Thread
+import numpy as np
+
+from compliant_control.control.state import State
+from compliant_control.mujoco.simulation import Simulation
+
+from compliant_control.dingo.dingo_driver import DingoDriver
+from compliant_control.dingo.dingo_driver_simulation import DingoDriverSimulation
+
+from compliant_control.kinova.kortex_client import KortexClient
+from compliant_control.kinova.kortex_client_simulation import KortexClientSimulation
+from compliant_control.kinova.utilities import DeviceConnection
+
+from compliant_control.control.calibration import Calibration
+
+from user_interface_msg.msg import Ufdbk, Ucmd, Ustate, Utarget, Record, Data
+
+PUBLISH_RATE = 100
+D_MAX_RESET = 0.001
+
+
+class ControlInterfaceNode:
+    """A node that starts the control interface of the robot."""
+
+    def __init__(self, args) -> None:
+        rospy.init_node("control_interface_node")
+        self.simulate = "--simulate" in args
+
+        self.pub_fdbk = rospy.Publisher("/feedback", Ufdbk, queue_size=10)
+        self.pub_state = rospy.Publisher("/state", Ustate, queue_size=10)
+        self.pub_record = rospy.Publisher("/record", Record, queue_size=10)
+        self.pub_calibration = rospy.Publisher("/calibration", Data, queue_size=10)
+        rospy.Subscriber("/command", Ucmd, self.handle_input, queue_size=10)
+        rospy.Subscriber("/target", Utarget, self.update_target, queue_size=10)
+
+        self.automove_target = False
+        self.state = State(self.simulate)
+
+        if self.simulate:
+            self.start_simulation()
+        else:
+            self.start_robot()
+
+    def start_threads(self) -> None:
+        """Start the threads."""
+        spin_thread = Thread(target=self.start_spin_loop)
+        spin_thread.start()
+
+        publish_thread = Thread(target=self.start_publish_loop)
+        publish_thread.start()
+
+    def start_robot(self) -> None:
+        """Start the robot."""
+        self.dingo = DingoDriver(self.state)
+        self.dingo.log = rospy.loginfo
+        self.start_threads()
+        with DeviceConnection.createTcpConnection() as router, DeviceConnection.createUdpConnection() as real_time_router:
+            self.kinova = KortexClient(
+                self.state, router=router, real_time_router=real_time_router
+            )
+            self.kinova.log = rospy.loginfo
+            self.calibration = Calibration(
+                self.state, self.kinova, self.publish_calibration
+            )
+            self.calibration.log = rospy.loginfo
+            signal.signal(signal.SIGINT, self.kinova.stop)
+            self.kinova.start()
+
+    def start_simulation(self) -> None:
+        """Start the simulation."""
+        self.simulation = Simulation(self.state)
+        self.dingo = DingoDriverSimulation(self.state, self.simulation)
+        self.kinova = KortexClientSimulation(self.state, self.simulation)
+        self.kinova.log = rospy.loginfo
+        self.kinova.start_in_new_thread()
+        self.calibration = Calibration(
+            self.state, self.kinova, self.publish_calibration
+        )
+        self.calibration.log = rospy.loginfo
+        self.start_threads()
+        self.simulation.start()
+
+    def start_publish_loop(self) -> None:
+        """Start a loop that publishes the feedback."""
+        while not hasattr(self, "kinova"):
+            time.sleep(0.5)
+        rospy.loginfo("READY!")
+        while True:
+            self.publish_feedback()
+            self.publish_record()
+            time.sleep(1 / PUBLISH_RATE)
+
+    def publish_feedback(self) -> None:
+        """Publish feedback."""
+        feedback = Ufdbk()
+        feedback.kinova_pos = list(self.state.kinova_feedback.q)
+        feedback.kinova_vel = list(self.state.kinova_feedback.dq)
+        feedback.kinova_tor = list(self.state.kinova_feedback.c)
+        feedback.kinova_rate = self.kinova.rate_counter.rate
+        feedback.dingo_pos = list(self.state.dingo_feedback.q)
+        feedback.dingo_vel = list(self.state.dingo_feedback.dq)
+        feedback.dingo_tor = list(self.state.dingo_feedback.c)
+        feedback.dingo_rate = self.dingo.rate_counter.rate
+        feedback.controller_rate = self.state.controller.rate_counter.rate
+        self.pub_fdbk.publish(feedback)
+
+    def publish_record(self) -> None:
+        """Publish data to record."""
+        msg = Record()
+        msg.pos_x = list(self.state.x)
+        msg.pos_t = list(self.state.target)
+        msg.pos_q = list(self.state.kinova_feedback.q)
+        msg.cur_comp = list(self.state.controller.c_compliant)
+        msg.cur_null = list(self.state.controller.c_nullspace)
+        msg.cur_fric = list(self.state.controller.c_compensate)
+        msg.cur_fb = list(self.state.kinova_feedback.c)
+        self.pub_record.publish(msg)
+
+    def publish_calibration(self, data: np.ndarray) -> None:
+        """Publish the data."""
+        msg = Data()
+        msg.data = list(data)
+        self.pub_calibration.publish(msg)
+
+    def handle_input(self, msg: Ucmd) -> None:
+        """Handle user input."""
+        cmd = msg.command
+        if cmd == "Refresh":
+            pass
+        elif cmd == "Home":
+            self.kinova.home()
+        elif cmd == "Zero":
+            self.kinova.zero()
+        elif cmd == "Retract":
+            self.kinova.retract()
+        elif cmd == "Pref":
+            self.kinova.pref()
+        elif cmd == "Start HLT":
+            if self.target_is_at_end_effector():
+                self.kinova.start_HLT()
+        elif cmd == "Stop HLT":
+            self.kinova.stop_HLT()
+        elif cmd == "Start LLC":
+            self.kinova.start_LLC()
+        elif cmd == "Stop LLC":
+            self.kinova.stop_LLC()
+        elif cmd == "Start LLC Task":
+            if self.target_is_at_end_effector():
+                self.kinova.connect_LLC()
+        elif cmd == "Stop LLC Task":
+            self.kinova.disconnect_LLC()
+        elif cmd == "Clear":
+            self.kinova.clear_faults()
+        elif cmd == "Open":
+            self.kinova._move_gripper(close=False)
+        elif cmd == "Close":
+            self.kinova._move_gripper(close=True)
+        elif cmd == "Automove":
+            self.toggle_automove_target()
+        elif cmd == "Reset":
+            if self.simulate:
+                self.simulation.reset_target()
+        elif cmd in [str(n) for n in range(self.kinova.actuator_count)]:
+            self.kinova.toggle_active(int(cmd))
+        elif cmd in ["grav", "fric", "arm", "null", "base"]:
+            if cmd == "arm" and not self.target_is_at_end_effector():
+                pass
+            else:
+                self.state.controller.toggle(cmd)
+        elif cmd == "Move Dingo":
+            self.state.controller.command_base_direction(msg.args, 0.6)
+        elif cmd == "Calibrate":
+            self.calibration.calibrate_all()
+        else:
+            print(f"Service call {cmd} is unknown.")
+        self.publish_state()
+
+    def publish_state(self) -> None:
+        """Publish the state of the robot."""
+        state = Ustate()
+        state.servoing_mode = self.kinova.mode
+        state.comp_grav = self.state.controller.comp_grav
+        state.comp_fric = self.state.controller.comp_fric
+        state.imp_arm = self.state.controller.imp_arm
+        state.imp_null = self.state.controller.imp_null
+        state.imp_base = self.state.controller.imp_base
+
+        state.active = self.kinova.joint_active
+        state.mode = self.kinova.get_control_modes()
+        state.ratio = list(self.state.ratios)
+        state.friction = list(self.state.frictions)
+        state.automove_target = self.automove_target
+
+        self.pub_state.publish(state)
+
+    def target_is_at_end_effector(self) -> None:
+        """Reset the target."""
+        if self.simulate:
+            self.simulation.reset_target()
+            self.state.target = self.state.x
+        return np.linalg.norm(self.state.target - self.state.x) < D_MAX_RESET
+
+    def update_target(self, msg: Utarget) -> None:
+        """Update the target."""
+        self.state.target = np.array(msg.target)
+
+    def toggle_automove_target(self) -> None:
+        """Toggle automove of target."""
+        if self.simulate:
+            self.simulation.target_mover.toggle()
+            self.automove_target = self.simulation.target_mover.active
+        else:
+            self.automove_target = not self.automove_target
+
+    def step_callback(self) -> None:
+        """Called every simulation step."""
+        self.state.target = self.simulation.relative_target
+
+    def start_spin_loop(self) -> None:
+        """Start node spinning."""
+        rospy.spin()
+
+
+def main(args: any = None) -> None:
+    """Main."""
+    args = rospy.myargv(argv=sys.argv)
+    ControlInterfaceNode(args)
+    os._exit(0)
+
+
+if __name__ == "__main__":
+    main()
---
+++ ros/noetic/src/user_interface/user_interface/user_interface_node.py
@@ -0,0 +1,135 @@
+import os
+import rospy
+from threading import Thread
+import time
+import sys
+
+from user_interface_msg.msg import Ufdbk, Ustate, Ucmd, Utarget
+
+from compliant_control.interface.user_interface import UserInterface
+from compliant_control.mujoco.visualization import Visualization
+
+from compliant_control.utilities.rate_counter import RateCounter
+
+
+class UserInterfaceNode:
+    """A node that starts the user interface."""
+
+    def __init__(self, args) -> None:
+        rospy.init_node("user_interface_node")
+        rospy.Subscriber("/feedback", Ufdbk, self.feedback, queue_size=10)
+        rospy.Subscriber("/state", Ustate, self.state, queue_size=10)
+        self.pub_command = rospy.Publisher("/command", Ucmd, queue_size=10)
+        self.pub_target = rospy.Publisher("/target", Utarget, queue_size=10)
+
+        self.interface = UserInterface(self.command)
+        self.interface.create_ui()
+
+        self.visualize = "--visualize" in args
+
+        if self.visualize:
+            self.visualization = Visualization()
+            visualize_thread = Thread(target=self.visualization.start)
+            visualize_thread.start()
+            target_thread = Thread(target=self.publish_target_loop)
+            target_thread.start()
+
+        spin_thread = Thread(target=self.start_spin_loop)
+        spin_thread.start()
+
+        self.interface.start()
+
+    def command(self, command: str, args: list = None) -> None:
+        """Send a command to the control interface."""
+        self.interface.update_state("waiting")
+        if self.visualize:
+            if command == "Automove":
+                self.visualization.target_mover.toggle()
+            if command == "Reset":
+                self.visualization.reset_target()
+                self.interface.reset_wheels()
+            if command in ["Start LLC Task", "arm", "Start HLT"]:
+                self.visualization.reset_target()
+                self.publish_target()
+                time.sleep(0.1)
+        msg = Ucmd()
+        msg.command = command
+        if args is not None:
+            msg.args = args
+        self.pub_command.publish(msg)
+
+    def feedback(self, msg: Ufdbk) -> None:
+        """Process the feedback."""
+        self.interface.rates.con = msg.controller_rate
+        if len(msg.kinova_pos) > 0:
+            self.kinova_feedback(msg)
+        if len(msg.dingo_pos) > 0:
+            self.dingo_feedback(msg)
+
+    def state(self, msg: Ustate) -> None:
+        """Update the state."""
+        self.interface.state.mode = msg.servoing_mode
+        self.interface.state.comp_grav = msg.comp_grav
+        self.interface.state.comp_fric = msg.comp_fric
+        self.interface.state.imp_arm = msg.imp_arm
+        self.interface.state.imp_null = msg.imp_null
+        self.interface.state.imp_base = msg.imp_base
+        self.interface.state.automove_target = msg.automove_target
+        for n, joint in enumerate(self.interface.joints):
+            joint.active = msg.active[n]
+            joint.mode = msg.mode[n]
+            joint.ratio = msg.ratio[n]
+            joint.friction = msg.friction[n]
+        self.interface.update_state()
+
+    def kinova_feedback(self, msg: Ufdbk) -> None:
+        """Process the Kinova feedback."""
+        for joint in self.interface.joints:
+            joint.pos = msg.kinova_pos[joint.index]
+            joint.vel = msg.kinova_vel[joint.index]
+            joint.eff = msg.kinova_tor[joint.index]
+        self.interface.rates.kin = msg.kinova_rate
+        self.interface.update_bars("Kinova")
+        if self.visualize:
+            self.visualization.set_qpos_value("Kinova", "position", msg.kinova_pos)
+
+    def dingo_feedback(self, msg: Ufdbk) -> None:
+        """Process the Dingo feedback."""
+        for wheel in self.interface.wheels:
+            last_position = wheel.pos
+            wheel.encoder_position = msg.dingo_pos[wheel.index]
+            wheel.vel = wheel.pos - last_position
+            wheel.eff = msg.dingo_tor[wheel.index]
+        self.interface.rates.din = msg.dingo_rate
+        self.interface.update_bars("Dingo")
+        if self.visualize:
+            self.visualization.set_qpos_value("Dingo", "position", msg.dingo_pos)
+
+    def publish_target_loop(self) -> None:
+        """A loop that publishes the target."""
+        rate_counter = RateCounter(30)
+        while True:
+            self.publish_target()
+            rate_counter.count()
+            rate_counter.sleep()
+
+    def publish_target(self) -> None:
+        """Publish the target."""
+        msg = Utarget()
+        msg.target = list(self.visualization.relative_target)
+        self.pub_target.publish(msg)
+
+    def start_spin_loop(self) -> None:
+        """Start node spinning."""
+        rospy.spin()
+
+
+def main(args: any = None) -> None:
+    """Main."""
+    args = rospy.myargv(argv=sys.argv)
+    UserInterfaceNode(args)
+    os._exit(0)
+
+
+if __name__ == "__main__":
+    main()
